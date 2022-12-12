6f5804d22e6c643be7890d5543cb2ff8c7f79d0e
Gabriele D'Aleo <daleogabdc@gmail.com>
2022-12-12 15:20:54 +0100

    First migrations

---
+++ Betazon/Migrations/20221212141701_Users.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Betazon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Credentials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Credentials_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "SalesLT",
+                        principalTable: "Customer",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserLogs_Credentials_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Credentials",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credentials_CustomerId",
+                table: "Credentials",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogs_UserId",
+                table: "UserLogs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserLogs");
+
+            migrationBuilder.DropTable(
+                name: "Credentials");
+        }
+    }
+}
---
+++ Hashing/Encrypt.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing
+{
+    public class Encrypt
+    {
+       
+            #region Metodi
+
+            /// <summary>
+            /// Hashing method
+            /// </summary>
+            /// <param name="data">data to hash such as password</param>
+            /// <param name="salt">salt to add to the data associated with user</param>
+            /// <returns>the hash of the combination between data and salt</returns>
+            public string Hash(string data, string salt)
+            {
+                SHA256 sha256 = SHA256.Create();
+                sha256.ComputeHash(Encoding.UTF8.GetBytes(data + salt));
+
+                return Convert.ToBase64String(sha256.Hash);
+            }
+
+
+            /// <summary>
+            /// Random salt generator 
+            /// </summary>
+            /// <returns>An 8 character string ending with =</returns>
+            public string SaltGenerator()// RANDOM SALT GENERATOR
+            {
+                string salt = "";
+                Random random = new Random();
+                bool flag;
+                int value;
+
+                for (int i = 0; i < 7; i++)
+                {
+                    do
+                    {
+                        flag = false;
+                        value = random.Next(33, 125);// TABELLA ASCII
+                        if (value == 61)
+                            flag = true;
+                    }
+                    while (flag);
+
+                    salt += (char)value;// CONVERTIAMO IL VALORE INTERO IN ASCII
+                }
+
+                return salt + "=";// AGGIUNGIAMO ALLA STRINGA = stringadi 8 caratteri
+            }
+
+            #endregion
+    }
+    
+}
